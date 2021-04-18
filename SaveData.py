068b885ecea3c1b0e2891ae08c86c55d019afee3
akshatmehta1906 <akshatmehta1906@gmail.com>
2021-04-18 14:21:05 +0530

    Update SaveData.py
    ..

--- SaveData.py
+++ SaveData.py
@@ -4,7 +4,7 @@ url = "https://weathercast-arduino.herokuapp.com/saveArduino"
 
 data = {
     "api_key": "ArduinoApiKey@00191CA7",
-    "data": ["12345", "98756", "879654", "121212", "111111"]
+    "data": ["12345", "98756", "879654", "565656", "111111", "222222"]
 }
 
 x = requests.post(url, data = data)
